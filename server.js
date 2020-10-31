91cd3c246451f2e321c01b1165e2212b32089205
dorpi <dorpitaro@gmail.com>
2020-10-31 17:35:06 +0200

    Change for herolu

--- server.js
+++ server.js
@@ -112,11 +112,6 @@ app.use('/api/post',post);
 
 
 
-if(process.env.NODE_ENV === 'production'){
-  app.get('/*',(req,res)=>{
-      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
-}
 
 //Server static assets if in production
 if (process.env.NODE_ENV==='production'){
